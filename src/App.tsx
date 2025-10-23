86d30b2cdefb9638c3e3bb8efa2ad5720e107fcf
Sthefanyom <om.sthefany@gmail.com>
2025-10-23 16:11:56 -0300

    Fazendo alterações no Blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
 
 function App() {
-  return (<Home />);
+  return ( <> 
+  <Navbar /> 
+  <Home />
+  <Footer />
+  </>
+  );
 }
 
 export default App;
