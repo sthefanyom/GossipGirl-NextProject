17c1bb4f434d9665aabe5a5826910b5a4ced472b
Sthefanyom <om.sthefany@gmail.com>
2025-10-31 18:50:44 -0300

    Corrigindo Navbar

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,18 @@
 import {GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
+import { useContext, type ReactNode } from "react"
+import { AuthContext } from "../../contexts/AuthContext"
 
 function Footer() {
+    
+    // eslint-disable-next-line prefer-const
+    let data = new Date().getFullYear()
+    const { usuario } = useContext(AuthContext)
 
-    const data = new Date().getFullYear()
+    let component:  ReactNode
 
-    return (
-        <>
+    if (usuario.token !== "") {
+
+        component = (
             <div className="flex justify-center bg-black text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className="text-xl font-bold">
@@ -34,6 +41,13 @@ function Footer() {
                     </div>
                 </div>
             </div>
+
+        )
+    }
+
+    return (
+        <>
+            { component }
         </>
     )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,22 @@
-import { useContext } from "react"
+import { useContext, type ReactNode } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext"
 
 function Navbar() {
 
     const navigate = useNavigate()
-    const { handleLogout } = useContext(AuthContext)
+    const { usuario, handleLogout } = useContext(AuthContext)
     function logout() {
         handleLogout()
         alert(" O Usuário foi desconectado com sucesso!")
         navigate('/')
     }
-    
-    return (
-        <>
+
+     let component: ReactNode
+
+     if (usuario.token !== "") {
+
+        component = (
             <div className='w-full flex justify-center py-4 bg-black text-white'>
                 <div className="container flex justify-between text-lg mx-8">
                     <Link 
@@ -32,6 +35,13 @@ function Navbar() {
                     </div>
                 </div>
             </div>
+
+        )
+     }
+
+    return (
+        <>
+            { component }
         </>
     )
 }
